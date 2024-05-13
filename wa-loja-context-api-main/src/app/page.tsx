bb6b8d91a6c832fe2f09736a44d2cafc160dd79d
gabrielfreitas1406 <gabriel.freitas@icomp.ufam.edu.br>
2024-05-13 13:39:08 -0400

    iniciando o segundo trabalho de gerenciamento de estadp

--- wa-loja-context-api-main/src/app/page.tsx
+++ wa-loja-context-api-main/src/app/page.tsx
@@ -1,14 +1,20 @@
 "use client";
 
-import { useState } from "react";
+import React, { createContext, useState } from "react";
 import ListagemProdutos from "./components/ListagemProdutos/ListagemProdutos";
 import { mockProdutos } from "./mocks/produtos";
+
+export const Favoritos_contexto = createContext({
+  favoritos: [] as Produto[],
+  setFavoritos:  {} as React.Dispatch<React.SetStateAction<Produto[]>>
+});
 
 export default function App() {
   const produtos = mockProdutos;
   const [favoritos, setFavoritos] = useState<Produto[]>([]);
 
   return (
+    <Favoritos_contexto.Provider value={{favoritos, setFavoritos}}>
     <main>
       <div className="container p-5">
         <ListagemProdutos
@@ -18,5 +24,6 @@ export default function App() {
         />
       </div>
     </main>
+    </Favoritos_contexto.Provider>
   );
 }
