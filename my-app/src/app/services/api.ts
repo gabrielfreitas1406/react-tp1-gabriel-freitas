c656a0105feecc71e050b3337c83f65299176eb6
gabrielfreitas1406 <gabriel.freita@icomp.ufam.edu.br>
2024-05-14 18:38:06 -0400

    selecionando só o wa-loja-context-api-main

--- my-app/src/app/services/api.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios"
-
-const api = axios.create({
-    baseURL: "https://ranekapi.origamid.dev/json/api",
-});
-
-export default api;
--- my-app/src/app/services/produto.ts
+++
@@ -1,5 +0,0 @@
-import api from "./api";
-
-export default async function getDetalhesProduto(nomeProduto: string) : Promise<Produto>{
-    return api.get(`/produto/${nomeProduto}`).then((response)=> response.data);
-}
